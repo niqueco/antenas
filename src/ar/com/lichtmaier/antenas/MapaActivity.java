90daff0f4e59b50e41dee25d546b7cb8cc158769
Nicolás Lichtmaier <Nico.Lichtmaier@gmail.com>
2014-04-16 23:56:18 -0300

    Salvo un NPE.

--- src/ar/com/lichtmaier/antenas/MapaActivity.java
+++ src/ar/com/lichtmaier/antenas/MapaActivity.java
@@ -2,8 +2,10 @@ package ar.com.lichtmaier.antenas;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.view.*;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -71,14 +73,21 @@ public class MapaActivity extends ActionBarActivity
 		public void onActivityCreated(Bundle savedInstanceState)
 		{
 			super.onActivityCreated(savedInstanceState);
-			mapa = ((SupportMapFragment)getActivity().getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+			FragmentActivity act = getActivity();
+			mapa = ((SupportMapFragment)act.getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+			if(mapa == null)
+			{
+				Toast.makeText(act, "Uhm... por alguna razón no se pudo inicializar el mapita.", Toast.LENGTH_SHORT).show();
+				act.finish();
+				return;
+			}
 			mapa.setMyLocationEnabled(true);
 			mapa.moveCamera(CameraUpdateFactory.zoomTo(10));
 			if(AntenaActivity.coordsUsuario != null)
 				mapa.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(AntenaActivity.coordsUsuario.getLatitude(), AntenaActivity.coordsUsuario.getLongitude())));
-			for(Antena antena : Antena.dameAntenas(getActivity()))
+			for(Antena antena : Antena.dameAntenas(act))
 				mapa.addMarker(new MarkerOptions().position(antena.getLatLng()).title(antena.toString()).icon(BitmapDescriptorFactory.fromResource(R.drawable.antena)));
-			getActivity().findViewById(R.id.map).post(new Runnable() {
+			act.findViewById(R.id.map).post(new Runnable() {
 				@Override
 				public void run()
 				{
