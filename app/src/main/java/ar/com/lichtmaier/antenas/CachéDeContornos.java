3280a68da0c0cd40fde8ff07f5cdc8294460f9c4
Nicolás Lichtmaier <Nico.Lichtmaier@gmail.com>
2015-10-07 20:41:39 -0300

    Agrego clases para obtener, usar y guardar localmente los contornos que ofrece la FCC de EE.UU.

---
+++ app/src/main/java/ar/com/lichtmaier/antenas/CachéDeContornos.java
@@ -0,0 +1,174 @@
+package ar.com.lichtmaier.antenas;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.util.LruCache;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CachéDeContornos
+{
+	public static final int VERSION_BASE = 1;
+
+	private static CachéDeContornos instancia;
+
+	final private SQLiteDatabase db;
+
+	final private LruCache<Integer, Polígono> lruCache = new LruCache<>(50);
+	private final XmlPullParserFactory xmlPullParserFactory;
+
+	public static CachéDeContornos dameInstancia(Context ctx)
+	{
+		if(instancia == null)
+		{
+			synchronized(CachéDeContornos.class)
+			{
+				if(instancia == null)
+					instancia = new CachéDeContornos(ctx);
+			}
+		} else
+		{
+			instancia.db.acquireReference();
+		}
+		return instancia;
+	}
+
+	public void devolver()
+	{
+		synchronized(CachéDeContornos.class)
+		{
+			db.releaseReference();
+			if(!db.isOpen())
+			{
+				instancia = null;
+				lruCache.evictAll();
+			}
+		}
+	}
+
+	private CachéDeContornos(Context ctx)
+	{
+		db = SQLiteDatabase.openDatabase(ctx.getExternalCacheDir() + "/contornos.db", null, SQLiteDatabase.CREATE_IF_NECESSARY);
+
+		if(db.getVersion() != VERSION_BASE)
+		{
+			db.execSQL("create table contorno ("
+					+ "app_id integer primary key on conflict replace, "
+					+ "poligono blob not null, "
+					+ "ult_uso integer not null, "
+					+ "fecha_obtenido integer not null"
+					+ ")");
+			db.setVersion(VERSION_BASE);
+		}
+
+		try
+		{
+			xmlPullParserFactory = XmlPullParserFactory.newInstance();
+		} catch(XmlPullParserException e)
+		{
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	Polígono dameContornoFCC(int appId) throws IOException
+	{
+		Polígono contorno = lruCache.get(appId);
+		if(contorno == null)
+		{
+			String selection = "app_id=?";
+			String[] selectionArgs = {Integer.toString(appId)};
+			Cursor cursor = db.query("contorno", new String[] {"poligono"}, selection, selectionArgs, null, null, null);
+			try
+			{
+				if(cursor.moveToFirst())
+					contorno = Polígono.deBytes(cursor.getBlob(0));
+			} finally
+			{
+				cursor.close();
+			}
+			if(contorno != null)
+			{
+				ContentValues values = new ContentValues(1);
+				values.put("ult_uso", (int)(System.currentTimeMillis() / 1000L));
+				db.update("contorno", values, selection, selectionArgs);
+			}
+		}
+		if(contorno == null)
+		{
+			Log.i("antenas", "buscando el polígono con appId=" + appId);
+			InputStream in = new URL("http://transition.fcc.gov/fcc-bin/contourplot.kml?appid=" + appId).openStream();
+			try
+			{
+				XmlPullParser parser = xmlPullParserFactory.newPullParser();
+				parser.setInput(in, "UTF-8");
+				int t;
+				boolean inLineString = false;
+				while((t=parser.next()) != XmlPullParser.END_DOCUMENT)
+				{
+					if(t == XmlPullParser.START_TAG && parser.getName().equals("LineString"))
+					{
+						inLineString = true;
+						continue;
+					}
+					if(t == XmlPullParser.END_TAG && parser.getName().equals("LineString"))
+					{
+						inLineString = false;
+						continue;
+					}
+					if(inLineString && t == XmlPullParser.START_TAG && parser.getName().equals("coordinates"))
+					{
+						t = parser.next();
+						if(t != XmlPullParser.TEXT)
+						{
+							Log.e("antenas", "No se encontró el texto esperado en la línea " + parser.getLineNumber() + " de contorno appid=" + appId);
+							return null;
+						}
+
+						Pattern pat = Pattern.compile("^\\s*([^,]+),([^,]+),.*$", Pattern.MULTILINE);
+
+						Polígono.Builder pb = new Polígono.Builder();
+
+						Matcher m = pat.matcher(parser.getText());
+						while(m.find())
+						{
+							float lat = Float.parseFloat(m.group(2));
+							float lon = Float.parseFloat(m.group(1));
+							pb.add(new LatLng(lat, lon));
+						}
+						contorno = pb.build();
+						lruCache.put(appId, contorno);
+						ContentValues values = new ContentValues(3);
+						values.put("app_id", appId);
+						values.put("poligono", contorno.aBytes());
+						int ahora = (int)(System.currentTimeMillis() / 1000L);
+						values.put("ult_uso", ahora);
+						values.put("fecha_obtenido", ahora);
+						db.insert("contorno", null, values);
+						break;
+					}
+				}
+			} catch(XmlPullParserException e)
+			{
+				throw new RuntimeException(e);
+			} finally
+			{
+				in.close();
+			}
+		}
+		return contorno;
+	}
+}
---
+++ app/src/main/java/ar/com/lichtmaier/antenas/Polígono.java
@@ -0,0 +1,94 @@
+package ar.com.lichtmaier.antenas;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.maps.android.PolyUtil;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Polígono
+{
+	final List<LatLng> puntos;
+	private final LatLngBounds bounds;
+
+	public Polígono(List<LatLng> puntos, LatLngBounds bounds)
+	{
+		this.puntos = puntos;
+		this.bounds = bounds;
+	}
+
+	public boolean contiene(LatLng punto)
+	{
+		return bounds.contains(punto) && PolyUtil.containsLocation(punto, puntos, false);
+	}
+
+	public LatLngBounds getBoundingBox()
+	{
+		return bounds;
+	}
+
+	public List<LatLng> getPuntos()
+	{
+		return Collections.unmodifiableList(puntos);
+	}
+
+	public byte[] aBytes()
+	{
+		try
+		{
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			DataOutputStream s = new DataOutputStream(bos);
+			for(LatLng punto : puntos)
+			{
+				s.writeFloat((float)punto.latitude);
+				s.writeFloat((float)punto.longitude);
+			}
+			s.close();
+			return bos.toByteArray();
+		} catch(IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static Polígono deBytes(byte[] blob)
+	{
+		DataInputStream s = new DataInputStream(new ByteArrayInputStream(blob));
+		try
+		{
+			Builder builder = new Builder();
+			int n = blob.length / (Float.SIZE / 8) / 2;
+			for(int i = 0 ; i < n; i++)
+			{
+				float lat = s.readFloat();
+				float lon = s.readFloat();
+				builder.add(new LatLng(lat, lon));
+			}
+			return builder.build();
+		} catch(IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	static class Builder
+	{
+		final private LatLngBounds.Builder boundsBuilder = new LatLngBounds.Builder();
+		final List<LatLng> puntos = new ArrayList<>();
+
+		public Builder add(LatLng punto)
+		{
+			boundsBuilder.include(punto);
+			puntos.add(punto);
+			return this;
+		}
+
+		public Polígono build()
+		{
+			return new Polígono(puntos, boundsBuilder.build());
+		}
+	}
+}
