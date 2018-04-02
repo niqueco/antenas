5c4cde7e6ad95f1d3856e6b4dd5f89c578f59baa
Nicolás Lichtmaier <nico.lichtmaier@gmail.com>
2018-04-02 10:50:06 -0300

    Agrego un test.

--- app/src/test/java/ar/com/lichtmaier/antenas/AntenaTest.java
+++ app/src/test/java/ar/com/lichtmaier/antenas/AntenaTest.java
@@ -1,5 +1,8 @@
 package ar.com.lichtmaier.antenas;
 
+import android.location.Location;
+
+import org.gavaghan.geodesy.GlobalCoordinates;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -19,5 +22,25 @@ public class AntenaTest
 		List<Antena> antenas = new ArrayList<>();
 		Antena.antenasEnRectángulo(RuntimeEnvironment.application, -34.559414, -58.459576, -34.631529, -58.372368, antenas);
 		assertEquals(2, antenas.size());
+	}
+
+	@Test
+	public void rumboDesde()
+	{
+		Antena antena = Antena.dameAntena(RuntimeEnvironment.application, País.AR, 1);
+
+		Location casa = new Location("*");
+		casa.setLatitude(-34.595479);
+		casa.setLongitude(-58.415715);
+
+		Location antenaLoc = new Location("*");
+		antenaLoc.setLatitude(antena.getLatLng().latitude);
+		antenaLoc.setLongitude(antena.getLatLng().longitude);
+
+		float z = casa.bearingTo(antenaLoc);
+		if(z < 0)
+			z += 360;
+		assertEquals(z, antena.rumboDesde(new GlobalCoordinates(-34.595479, -58.415715)), .0001);
+		assertEquals(casa.distanceTo(antenaLoc), antena.distanceTo(new GlobalCoordinates(-34.595479, -58.415715)), 1);
 	}
 }
